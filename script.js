bab48c240287aa46d1c3966d0915387b34d18412
fateme-hp <fatxnstrn@gmail.com>
2022-07-10 01:03:11 +0430

    calc

--- script.js
+++ script.js
@@ -27,25 +27,35 @@ function calcAge(e){
     console.log(hourDiff);
 
     let difference = Number(currentDate) - Number(birthDate);
+    console.log(difference);
     let output = new Date(difference);
     console.log(output);
 
 
-    let totalDays = difference / (1000*60*60*24);
-    console.log(totalDays);
+    let dayRemainder = difference % (1000*60*60*24);
+    let totalDays = difference - dayRemainder;
+    let dayCount = totalDays / (1000*60*60*24);
+    // console.log(totalDays);
+    console.log(dayCount);
 
-    let totalMonths = totalDays/ 30;
-    console.log(totalMonths);
+let yearRemainder = dayCount % 365;
+let totalYears =  dayCount - yearRemainder;
+let yearCount = totalYears / 365;
+console.log(yearCount);
 
-    let totalYears = totalMonths /12;
-    console.log(totalYears);
-
-
-
-
-
-
-
+    
+    let monthRemainder = yearRemainder % 30;
+    console.log(monthRemainder);
+    let totalMonths = yearRemainder - monthRemainder;
+    // console.log(totalMonths);
+    let monthCount = totalMonths / 30;
+    console.log(monthCount); 
+    if (monthRemainder > 0){
+        monthCount = monthCount - 1;
+        let remainingDays = 30 - monthRemainder;
+        console.log(remainingDays);
+    }
+  
     let result = document.createElement("div");
     result.classList.add("container");
     result.setAttribute('id','resultContainer')
