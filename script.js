bb4c4292eb4fa38d66597bbdbba85ad2a59315e8
fateme-hp <fatxnstrn@gmail.com>
2022-07-10 19:56:52 +0430

    adding more time details

--- script.js
+++ script.js
@@ -13,54 +13,55 @@ function calcAge(e){
     let currentDate = new Date();
 
 
-    console.log( birthDate);
-    console.log(currentDate);
-
 
     let yearDiff = currentDate.getFullYear() - birthDate.getFullYear();
-    console.log(yearDiff);
+
     let monthDiff = currentDate.getMonth() - birthDate.getMonth();
-    console.log(monthDiff);
-    let dayDiff = currentDate.getDate() - birthDate.getDate();
-    console.log(dayDiff);
-    let hourDiff = currentDate.getHours() - birthDate.getHours();
-    console.log(hourDiff);
-
-    let difference = Number(currentDate) - Number(birthDate);
-    console.log(difference);
-    let output = new Date(difference);
-    console.log(output);
-
-
-    let dayRemainder = difference % (1000*60*60*24);
-    let totalDays = difference - dayRemainder;
-    let dayCount = totalDays / (1000*60*60*24);
-    // console.log(totalDays);
-    console.log(dayCount);
-
-let yearRemainder = dayCount % 365;
-let totalYears =  dayCount - yearRemainder;
-let yearCount = totalYears / 365;
-console.log(yearCount);
-
-    
-    let monthRemainder = yearRemainder % 30;
-    console.log(monthRemainder);
-    let totalMonths = yearRemainder - monthRemainder;
-    // console.log(totalMonths);
-    let monthCount = totalMonths / 30;
-    console.log(monthCount); 
-    if (monthRemainder > 0){
-        monthCount = monthCount - 1;
-        let remainingDays = 30 - monthRemainder;
-        console.log(remainingDays);
+    if( monthDiff < 0){
+        yearDiff = yearDiff - 1;
+        monthDiff = 12 + monthDiff;
     }
+ 
+    let dayDiff = currentDate.getDate() - birthDate.getDate();
+    if ( dayDiff < 0){
+        monthDiff = monthDiff -1 ;
+        dayDiff = 30 + dayDiff;
+    }
+ 
+    let hourDiff = currentDate.getHours() - birthDate.getHours();
+    if ( hourDiff < 0 ){
+        dayDiff = dayDiff -1;
+        hourDiff = 24 + hourDiff;
+    } 
+
+    let minuteDiff = currentDate.getMinutes() - birthDate.getMinutes();
+    if (minuteDiff < 0 ){
+        hourDiff = hourDiff -1;
+        minuteDiff = 60 + minuteDiff;
+    } 
+  
+    let secondDiff = currentDate.getSeconds() - birthDate.getSeconds();
+    if ( secondDiff < 0 ){
+        minuteDiff = minuteDiff -1;
+        secondDiff = 60 + secondDiff;
+    } 
+    
+
+   
   
     let result = document.createElement("div");
     result.classList.add("container");
     result.setAttribute('id','resultContainer')
     document.querySelector("body").appendChild(result);
 
-    result.append(yearDiff +" years "+ monthDiff +" months " + dayDiff +" days ")
+   
+
+    result.append(
+        `
+        ${yearDiff} years, ${monthDiff} months, ${dayDiff} days,
+        ${hourDiff} hours, ${minuteDiff} minutes and ${secondDiff} seconds
+        
+        ` 
+        )
 
 }
