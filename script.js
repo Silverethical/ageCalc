c026fc0849248bdb68e65a0effe9b3b5c9351deb
fateme-hp <fatxnstrn@gmail.com>
2022-07-11 20:14:24 +0430

    comment

--- script.js
+++ script.js
@@ -4,51 +4,53 @@ calculateBtn.addEventListener("click",calcAge);
 
 function calcAge(e){
 
-    e.preventDefault();
+    e.preventDefault(); // prevent page from loading 
 
-    let birthday = document.getElementById("date").value;
+    let birthday = document.getElementById("date").value; // get the value of date input as birthday value
 
-    let birthDate = new Date(birthday);
+    let birthDate = new Date(birthday); // convert birthday value to the date 
 
-    let currentDate = new Date();
+    let currentDate = new Date(); // get the current date and time
 
 
-
+    // find  differences between birth date and current date
     let yearDiff = currentDate.getFullYear() - birthDate.getFullYear();
 
     let monthDiff = currentDate.getMonth() - birthDate.getMonth();
-    if( monthDiff < 0){
-        yearDiff = yearDiff - 1;
-        monthDiff = 12 + monthDiff;
-    }
- 
+   
     let dayDiff = currentDate.getDate() - birthDate.getDate();
-    if ( dayDiff < 0){
-        monthDiff = monthDiff -1 ;
-        dayDiff = 30 + dayDiff;
-    }
- 
+   
     let hourDiff = currentDate.getHours() - birthDate.getHours();
-    if ( hourDiff < 0 ){
-        dayDiff = dayDiff -1;
-        hourDiff = 24 + hourDiff;
-    } 
-
+ 
     let minuteDiff = currentDate.getMinutes() - birthDate.getMinutes();
-    if (minuteDiff < 0 ){
-        hourDiff = hourDiff -1;
-        minuteDiff = 60 + minuteDiff;
-    } 
-  
+   
     let secondDiff = currentDate.getSeconds() - birthDate.getSeconds();
+
+
+    // if there is a negative value , turn it to a positive value by mathematical calculations
     if ( secondDiff < 0 ){
         minuteDiff = minuteDiff -1;
         secondDiff = 60 + secondDiff;
     } 
-    
+    if (minuteDiff < 0 ){
+        hourDiff = hourDiff -1;
+        minuteDiff = 60 + minuteDiff;
+    } 
+    if ( hourDiff < 0 ){
+        dayDiff = dayDiff -1;
+        hourDiff = 24 + hourDiff;
+    } 
+    if ( dayDiff < 0){
+        monthDiff = monthDiff -1 ;
+        dayDiff = 30 + dayDiff;
+    }
+    if( monthDiff < 0){
+        yearDiff = yearDiff - 1;
+        monthDiff = 12 + monthDiff;
+    }
 
-   
   
+    // add result 
     let result = document.createElement("div");
     result.classList.add("container");
     result.setAttribute('id','resultContainer')
@@ -63,5 +65,6 @@ function calcAge(e){
         
         ` 
         )
+       
 
 }
